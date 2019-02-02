bb9f37c53fd3da25705f75d4daddc901772dcf1e
Josh Pospisil <josh.pospisil@gmail.com>
2019-02-02 13:55:21 -0600

    Added routes to retrieve county polygons to fake api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
 const stateAbbreviations = require('./state-abbreviations.json');
 const statePolygonData = require('./us-states.json');
+const countyPolygonData = require('./us-counties.json');
 const statePopulationData = require('./state-populations.json');
+
+const stateIdMap = statePolygonData.features.reduce((map, state) => {
+  const { NAME: stateName, STATE: id } = state.properties;
+  const stateInfo = stateAbbreviations.find(info => info.name.toLowerCase() === stateName.toLowerCase());
+  const { abbreviation } = stateInfo || {};
+  return {
+    [id]: abbreviation,
+    ...map,
+  };
+}, {});
 
 const statePolygons = statePolygonData.features.map((state) => {
   const { NAME: stateName } = state.properties;
-  const stateInfo = stateAbbreviations.find((info) => info.name.toLowerCase() === stateName.toLowerCase());
+  const stateInfo = stateAbbreviations.find(info => info.name.toLowerCase() === stateName.toLowerCase());
   const { abbreviation: id } = stateInfo || {};
   let { coordinates } = state.geometry || { coordinates: [] };
   const { type } = state.geometry || {};
@@ -21,16 +32,31 @@ const statePolygons = statePolygonData.features.map((state) => {
       lat: point[1],
       lng: point[0],
     }))),
-    // points: coordinates.map(point => ({
-    //   lat: point[1],
-    //   lng: point[0],
-    // })),
   };
 })
   .filter(state => state !== null);
 
+const countyPolygons = countyPolygonData.features.map((county) => {
+  const { STATE: stateId, NAME: countyName } = county.properties;
+  let { coordinates } = county.geometry || { coordinates: [] };
+  const { type } = county.geometry || {};
+
+  if (type === 'MultiPolygon') {
+    coordinates = [].concat(...coordinates);
+  }
+
+  return {
+    id: `${stateIdMap[stateId]}-${countyName.replace(' ', '-')}`,
+    statePolygonId: stateIdMap[stateId],
+    polygons: coordinates.map(polygon => polygon.map(point => ({
+      lat: point[1],
+      lng: point[0],
+    }))),
+  };
+});
+
 const statePopulations = statePopulationData.map((state) => {
-  let stateData = state;
+  const stateData = state;
   stateData.id = stateData.code;
   delete stateData.code;
   return stateData;
@@ -39,6 +65,7 @@ const statePopulations = statePopulationData.map((state) => {
 module.exports = () => {
   return {
     statePolygons,
+    countyPolygons,
     'census-data': statePopulations,
   };
 };
